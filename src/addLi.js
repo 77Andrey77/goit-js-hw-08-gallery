10455e8ed870daff02f6a78386f18dcc917b6c41
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2020-09-28 10:58:16 +0300

    основное задание

--- src/addLi.js
+++ src/addLi.js
@@ -1,6 +1,12 @@
 import galleryItems from './gallery-items.js';
 
-const galleryEl = document.querySelector('.gallery.js-gallery');
+const galleryEl = document.querySelector('.js-gallery');
+
+const modalEl = document.querySelector('.js-lightbox');
+
+const modalImageEl = document.querySelector('.lightbox__image');
+
+const closeButtonEl = document.querySelector('.lightbox__button');
 
 //создаю одну карту
 const makeImageCard = ({ preview, original, description }) => {
@@ -28,3 +34,24 @@ const elements = galleryItems.map(makeImageCard);
 // распыляем карты в разметку
 galleryEl.append(...elements);
 //////////////////////////////////////////////////////////////////////////////////////
+
+galleryEl.addEventListener('click', onOpenModal); //ставим слушателя на ul для открытия модалки
+closeButtonEl.addEventListener('click', onCloseModal); //став слуш на кнопку закрыть - закрытие модалки
+
+function onOpenModal(even) {
+    even.preventDefault(); // отмена действий браузера по умолчанию
+
+    if (even.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    modalEl.classList.add('is-open');
+    modalImageEl.src = even.target.dataset.source;
+    modalImageEl.alt = even.target.alt;
+}
+
+function onCloseModal(even) {
+    modalEl.classList.remove('is-open');
+    modalImageEl.src = '';
+    modalImageEl.alt = '';
+}
